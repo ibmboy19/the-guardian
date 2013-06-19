c2bb4355af697c2d2cd88397042fae85a265b807
ibmboy19 <ibmboy19@e9f9342b-de81-bb5c-d29f-6200f7e00299>
2013-06-19 04:04:57 +0000

    [Server] 1.build.xml 修正為  java 1.7 2.延遲偵測針對不同語言作業系統偵測

--- trunk/Server/src/guard/server/server/clientpacket/C_NetDelay.java
+++ trunk/Server/src/guard/server/server/clientpacket/C_NetDelay.java
@@ -1,18 +1,28 @@
 package guard.server.server.clientpacket;
 
 import guard.server.server.ClientProcess;
+import guard.server.server.model.instance.PlayerInstance;
 import guard.server.server.utils.NetDelayUtil;
 
 import java.io.IOException;
 
 public class C_NetDelay {
 	public C_NetDelay(ClientProcess _client) {
+		PlayerInstance pc = _client.getActiveChar();
+		if(pc == null){
+			return;
+		}
 		int delay = 0;
 		try {
 			delay = NetDelayUtil.netStatus(_client);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		if(pc.isInRoom()){
+			// TODO 傳送自己的Ping值給所有人
+			pc.getRoom().broadcastPacketToRoom("");
+		}
+		
 	}
-	// TODO 傳送延遲給所有遊戲室的玩家
+	
 }
