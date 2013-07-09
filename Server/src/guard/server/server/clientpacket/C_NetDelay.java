3045f8b963eec0d988b9b04ace29aef3e3f51ac9
boneache <boneache@e9f9342b-de81-bb5c-d29f-6200f7e00299>
2013-07-09 15:22:05 +0000

    [server tw]Ping Test

--- Server/src/guard/server/server/clientpacket/C_NetDelay.java
+++ Server/src/guard/server/server/clientpacket/C_NetDelay.java
@@ -1,7 +1,5 @@
 package guard.server.server.clientpacket;
 
-import static guard.server.server.clientpacket.ClientOpcodes.C_NetDelay;
-import static guard.server.server.clientpacket.ClientOpcodes.C_PacketSymbol;
 import guard.server.server.ClientProcess;
 import guard.server.server.model.GameRoom;
 import guard.server.server.model.instance.PlayerInstance;
@@ -18,22 +16,8 @@ public class C_NetDelay {
 		if (room == null)
 			return;
 
-		int delay = 0;
-		NetDelayUtil delayutil = new NetDelayUtil(_client);
-		while(!delayutil.isInterrupted()){
-			try {
-				Thread.sleep(50);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-		delay = delayutil.getResult();
-		// TODO 傳送自己的Ping值給所有人	
-		room.broadcastPacketToRoom(
-				String.valueOf(C_NetDelay) + C_PacketSymbol
-						+ pc.getAccountName() + C_PacketSymbol
-						+ String.valueOf(delay));
-
+		new NetDelayUtil(_client);
+		
 	}
 
 }
